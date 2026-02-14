fa918c30c8ee230792651f387d760eb5f5c4d069
lointlet <alexandre.flavio658@gmail.com>
2026-02-14 00:09:44 -0300

    feat: update image quality settings in next.config to enhance optimization and performance

--- next.config.ts
+++ next.config.ts
@@ -25,8 +25,8 @@ const nextConfig: NextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [480, 640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 31536000, // 1 ano de cache
-    qualities: [60, 75], // Next.js 15+
+    minimumCacheTTL: 31536000,
+    qualities: [40, 50, 60, 75],
     remotePatterns: [
       {
         protocol: 'https',
